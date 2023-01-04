47b25cf432c3158e68da71c76f5c02a8b939a12e
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-04 12:31:34 -0500

    Youtube Background Picker

---
+++ background chair/background.js
@@ -0,0 +1,33 @@
+document.getElementById('remote').onclick = colorOrage
+document.getElementById('red').onclick = colorRed
+document.getElementById('pink').onclick = colorPInk
+document.getElementById('green').onclick = colorGreen
+document.getElementById('blue').onclick = colorBlue
+
+
+function colorPInk() {
+document.querySelector('body').style.backgroundColor = '#ffc0cb'
+document.querySelector('body').style.color = 'white'
+document.getElementById('remote').style.backgroundColor = 'black'
+document.getElementById('remote').style.color = 'rgba(241, 81. 254, 1'
+}
+
+function colorGreen () {
+    document.querySelector('body').style.backgroundColor = 'green'
+    document.querySelector('body').style.color = 'white'
+}
+
+function colorBlue () {
+    document.querySelector('body').style.backgroundColor = 'blue'
+    document.querySelector('body').style.color = 'white'
+    
+}
+function colorRed () {
+    document.querySelector('body').style.backgroundColor = 'red'
+    document.querySelector('body').style.color = 'white'
+}
+
+function colorOrage () {
+    document.querySelector('h1').style.backgroundColor = 'orange'
+    document.querySelector('h1').style.color = 'white'
+}
