f4cc71012d6b0a5f6851c11f51082cab23f50c20
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-05 11:36:20 -0500

    Dog years convertor

---
+++ DOg years converter/convert.js
@@ -0,0 +1,14 @@
+let myAge = 100 //age
+let earlyYears = 2 //first two years
+earlyYears *= 10.5 //multipling it by first two dog years
+let laterYears = myAge - 2 //later two years
+laterYears *= 4 //multiply remeaning years by 4
+console.log(`later year ${laterYears}, and early years ${earlyYears}`)
+myAgeInDogYears = laterYears + earlyYears //calculating human years to dog ears
+
+let myName = "BATman"
+myName = myName.toLowerCase()
+console.log(myName.toLowerCase()) //convert string into lower case
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
