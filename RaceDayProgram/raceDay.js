e3e658cadc1a99b77497fef5f5587a09082c2d5d
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-07 09:26:05 -0500

    made small changes to the code

--- RaceDayProgram/raceDay.js
+++ RaceDayProgram/raceDay.js
@@ -1,6 +1,6 @@
 /* create a program that will randomly generate an age and assign it a race number. The age is between 1 and 100. People are of age of 18 years are not included in this race. Adults can sign up for two different session. If they do early registration. They will be the first one to run at 9:30 am. Adult who do not register will run at 11:00 am. Everyone else runs at 12:30 pm */
 
-let raceNumber = Math.floor(Math.random() * 1000);
+let raceNumber = Math.floor(Math.random(1) * 1000);
 let earlyRegister = true;
 let age = Math.floor(Math.random(1)*100);
 
