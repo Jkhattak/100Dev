9d147e9af16628672b98d7527e7503a5bae6d49e
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-06 20:52:16 -0500

    medal code

---
+++ weather tracker program/weatherTracker.js
@@ -0,0 +1,18 @@
+// create a program that keeps track of the way the environment changes with the seasons. Write a conditional statement to make this happen!
+
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if(season ==='winter') {
+  
+  console.log("It's winter! Everything is covered in snow.")
+} else if (season ==='fall') {
+  console.log('It\'s fall! Leaves are falling!')
+} else if (season==='summer') {
+  console.log("It's sunny and warm because it's summer!")
+}
+ else {
+  console.log('Invalid season.');
+}
+
