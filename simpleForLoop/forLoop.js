e79d89dc77773c30fbc9e3fcf33ed57cb03ca405
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-07 18:52:46 -0500

    reverse for loop

--- simpleForLoop/forLoop.js
+++ simpleForLoop/forLoop.js
@@ -6,3 +6,7 @@ for (let counter =5; counter <=100; counter++){
     console.log(counter)
   }
   
+  //Using for loop reverse
+  for (let counter = 3; counter >= 0; counter-- ){
+    console.log(counter)
+  }
