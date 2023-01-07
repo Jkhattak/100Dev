c4c07f010ce9ea9d06678156925da5deb59cb28b
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-07 18:13:27 -0500

    completed the simple for loop code

---
+++ simpleForLoop/forLoop.js
@@ -0,0 +1,8 @@
+// Write your code below
+//For loop contains three expressino inside the parentheses seperated by ;
+
+
+for (let counter =5; counter <=100; counter++){
+    console.log(counter)
+  }
+  
