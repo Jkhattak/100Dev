28b1ddc93f5dd35115a17f3d2f5ecb474bd9e3b1
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-07 11:08:21 -0500

    final code

---
+++ waterTreeDay/plantNeedsWater.js
@@ -0,0 +1,13 @@
+// Create a function to see if need to water the plant
+
+
+const plantNeedsWater = function (day) {
+    if (day ==='Wednesday') {
+      return true
+    } else {
+      return false
+    }
+  }
+ 
+  console.log(plantNeedsWater('Tuesday'))
+  
