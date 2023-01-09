5fd7d5a5894ceac092b188e140e32d7d06ba7d73
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-08 19:20:30 -0500

    map function

--- Iterator/Iterators.js
+++ Iterator/Iterators.js
@@ -1,5 +1,14 @@
+//For each funcitons
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
 
 fruits.forEach(fruit => console.log(`I want to eat a ${fruit}`))
+
+
+// Map function
+const numbers = [1, 2, 3, 4, 5]; 
+ 
+const bigNumbers = numbers.map(number => {
+  return number * 10;
+});
