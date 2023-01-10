53624d42c87faa89c61e76c26aaacb10af716e07
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-10 13:11:57 -0500

    modiefed the code

--- dayOfTheWeek/day.js
+++ dayOfTheWeek/day.js
@@ -1,6 +1,19 @@
 document.querySelector('button').addEventListener('click', run)
 
+
+
+
 function run() {
     document.querySelector('body').style.backgroundColor = 'pink'
-    document.querySelector('h3') 
+    let searchbar = document.querySelector('#searchbar').value
+    // document.querySelector('#display').innerText = searchbar
+    if (searchbar == 'tuesday' || searchbar =="Tuesday"){
+        alert(searchbar)
+    } else if (searchbar === 'thursday' || searchbar==='Thursday') {
+        console.log(searchbar)
+    } else {
+        console.log('BORING DAY')
+    }
+
+    
 }
