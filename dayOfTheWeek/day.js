9773d73415b2b5d01f9384eb769bb61ed44ad217
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-10 11:13:54 -0500

    fixing errors

--- dayOfTheWeek/day.js
+++ dayOfTheWeek/day.js
@@ -2,5 +2,5 @@ document.querySelector('button').addEventListener('click', run)
 
 function run() {
     document.querySelector('body').style.backgroundColor = 'pink'
-    document.querySelector('h3'). 
+    document.querySelector('h3') 
 }
