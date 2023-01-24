91b5e47992db0ae12c34eed57d6bb4c7de508e40
Jawad Khattal <Jawadahmad232@gmail.com>
2023-01-23 20:37:02 -0500

    more code

--- rapApi/server.js
+++ rapApi/server.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+let savage = {
+    'age': 28,
+    'birtName' : 'Sheyaa Bin Abrahim-Joseph',
+    'birthLocation' : 'London, England'
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 
+})
+
+app.get('/api/savage', (request, response) =>{
+    response.json
 })
 
 app.listen(port, ()=>{
