7453ca2c2353993d79b57b082353b6f6af0a4521
Jawad Khattak <jawadkhattak@Jawads-MacBook-Pro.local>
2023-01-24 08:58:49 -0500

    added more code

--- rapApi/server.js
+++ rapApi/server.js
@@ -6,10 +6,24 @@ const port = 8000
 app.use(cors())
 
 
-let savage = {
+let rapper = { 'savage': {
     'age': 28,
     'birtName' : 'Sheyaa Bin Abrahim-Joseph',
     'birthLocation' : 'London, England'
+
+}, 
+'Eminem' : {
+    'age': 46,
+    'birthname' : 'Martial Matther',  
+    'birthLocation': "Detriot,USA"
+
+
+}, 
+'Unknown' : {
+    'age' : "unknown",
+    'birthname': 30,
+    'birthname' : 'unknown'
+}
 }
 
 app.get('/', (request, response) => {
